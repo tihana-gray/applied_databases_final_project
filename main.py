182cd41e269beeca9414dd053e3533e1594f7458
Tihana <tihanagray64@gmail.com>
2026-04-18 12:20:54 +0100

    Testing the connection

--- main.py
+++ main.py
@@ -12,10 +12,22 @@ try:
 
     print("Connected successfully!")
 
-except Exception as e:
-    print("Error:", e)
-
 # 📚 References: 
 # - https://stackoverflow.com/questions/32754461/how-to-install-mysql-connector-via-pip
 # - https://www.w3schools.com/python/python_mysql_getstarted.asp
 # - https://www.geeksforgeeks.org/python/how-to-install-mysql-connector-package-in-python/
+
+# Running a simple query to test the connection
+    query = "SELECT * FROM attendee"
+
+    with conn:
+        cursor = conn.cursor()  
+        cursor.execute(query)
+
+        subjects = cursor.fetchall()
+
+        for s in subjects:
+            print(s)  
+# Prevents crashing the program and prints any errors that occur during the connection or query execution
+except Exception as e:
+    print("Error:", e)
